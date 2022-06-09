7c631c9fe391afd5d31959c9ab0d7c4a2bb9f3de
Ptimou <44651686+Ptimou@users.noreply.github.com>
2022-06-09 15:38:11 +0200

    Ajout BDD Locale

---
+++ AppConversionDesKMSFinale/Modèles/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppConversionDesKMSFinale.Modèles
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
---
+++ AppConversionDesKMSFinale/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using AppConversionDesKMSFinale.Modèles;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace AppConversionDesKMSFinale.Services
+{
+    public static class UserService
+    {
+        static SQLiteAsyncConnection db;
+        static async Task Init()
+        {
+            if (db != null)
+            {
+                return;
+            }
+            // Get an absolute path to the database file
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "SurfrunData.db");
+
+            db = new SQLiteAsyncConnection(databasePath);
+            await db.CreateTableAsync<User>();
+        }
+
+        public static async Task AddUser(string username, string password, string photoPath)
+        {
+            await Init();
+            var user = new User
+            {
+                Username = username,
+                Password = password,
+                PhotoPath = photoPath,
+            };
+            var id = await db.InsertAsync(user);
+        }
+
+        public static async Task RemoveUser(int id)
+        {
+            await Init();
+
+            await db.DeleteAsync<User>(id);
+        }
+
+        public static async Task<IEnumerable<User>> GetUser()
+        {
+            await Init();
+
+            var user = await db.Table<User>().ToListAsync();
+            return user;
+        }
+    }
+}
